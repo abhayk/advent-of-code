915ef459aeae3c00e0ca58dd658e2df613d00081
abkrishnan <abkrishnan@twilio.com>
2020-12-18 23:16:03 +0530

    Implemented part 2 of Day 16.

--- src/main/java/aoc2020/Day16.java
+++ src/main/java/aoc2020/Day16.java
@@ -2,12 +2,17 @@ package aoc2020;
 
 import common.Util;
 
+import java.awt.image.ImageProducer;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Queue;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Day16 {
     public static void main(String[] args) throws IOException {
@@ -31,7 +36,34 @@ public class Day16 {
             .mapToInt(i -> i)
             .sum());
 
+        // part 2
+        Integer[][] validTickets = nearbyTickets.stream()
+            .filter(t -> isTicketValid(t, rules))
+            .toArray(Integer[][]::new);
 
+        Queue<Integer> indexes = new ArrayDeque<>();
+        IntStream.range(0, validTickets[0].length).forEach(indexes::add);
+        List<Rule> tmpRules = new ArrayList<>(rules);
+        while(!indexes.isEmpty()) {
+            int index = indexes.poll();
+            Integer[] col = getColumnAsArray(validTickets, index);
+            List<Rule> matchingRules = tmpRules.stream()
+                .filter(r -> isArrayMatchingRule(col, r))
+                .collect(Collectors.toList());
+            if(matchingRules.size() == 1) {
+                Rule rule = matchingRules.get(0);
+                rule.index = index;
+                tmpRules.remove(rule);
+            } else {
+                indexes.add(index);
+            }
+        }
+
+        System.out.println(rules.stream()
+            .filter(r -> r.name.startsWith("departure"))
+            .map(r -> ourTicket[r.index])
+            .mapToLong(n -> n)
+            .reduce(1L, (a,b) -> a*b));
     }
 
     private static boolean isTicketValid(Integer[] ticket, List<Rule> rules) {
@@ -43,6 +75,18 @@ public class Day16 {
         return rules.stream()
             .flatMap(r -> r.ranges.stream())
             .anyMatch(range -> n >= range[0] && n <= range[1] );
+    }
+
+    private static boolean isArrayMatchingRule(Integer[] arr, Rule rule) {
+        return Arrays.stream(arr)
+            .allMatch(n -> rule.ranges.stream()
+                .anyMatch(range -> n >= range[0] && n <= range[1]));
+    }
+
+    private static Integer[] getColumnAsArray(Integer[][] input, int index) {
+        Integer[] col = new Integer[input.length];
+        IntStream.range(0, input.length).forEach(i -> col[i] = input[i][index]);
+        return col;
     }
 
     private static Integer[] parseTicket(String input) {
@@ -63,6 +107,7 @@ public class Day16 {
     }
 
     static class Rule {
+        int index;
         String name;
         List<Integer[]> ranges = new ArrayList<>();
     }
