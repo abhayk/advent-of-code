4538a4cadb0ffe173703916e53fc37c35fdde6eb
Abhay Krishnan <abhaykrishnan87@gmail.com>
2024-12-03 22:03:18 +0530

    Used a single regex for part 2.

--- src/main/kotlin/aoc2024/Day3.kt
+++ src/main/kotlin/aoc2024/Day3.kt
@@ -1,12 +1,11 @@
 package aoc2024
 
 import Solution
-import java.util.TreeMap
 
 class Day3: Solution {
-    private val mulRegex = """mul\((\d+),(\d+)\)""".toRegex()
 
     override fun part1(input: String): Any {
+        val mulRegex = """mul\((\d+),(\d+)\)""".toRegex()
         return mulRegex.findAll(input).sumOf {
             val (left, right) = it.destructured
             left.toLong() * right.toLong()
@@ -14,16 +13,21 @@ class Day3: Solution {
     }
 
     override fun part2(input: String): Any {
-        val doRegex = """do\(\)""".toRegex()
-        val dontRegex = """don't\(\)""".toRegex()
-        val treeMap = TreeMap<Int, String>()
-        doRegex.findAll(input).forEach { treeMap[it.range.first] = "DO" }
-        dontRegex.findAll(input).forEach { treeMap[it.range.first] = "DONT" }
-        return mulRegex.findAll(input)
-            .filter { (treeMap.floorEntry(it.range.first)?.value ?: "DO") == "DO" }
-            .sumOf {
-                val (left, right) = it.destructured
-                left.toLong() * right.toLong()
+        val regex = """(?<do>do\(\))|(?<dont>don't\(\))|mul\((\d+),(\d+)\)""".toRegex()
+        var enabled = true
+        var sum = 0L
+        regex.findAll(input).forEach {
+            when {
+                it.groups["do"] != null -> enabled = true
+                it.groups["dont"] != null -> enabled = false
+                else -> {
+                    if (enabled) {
+                        val(_, _, left, right) = it.destructured
+                        sum += left.toLong() * right.toLong()
+                    }
+                }
             }
+        }
+        return sum
     }
 }
